fad6991f225c78e9392d5d4204d73015e51137e8
valentinomazzoca <valenmazzoca89@gmail.com>
2026-09-08 14:28:36 -0300

    comunicacion back y base de datos

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+const pool = require("./config/database");
 const app = express();
 
 app.use(cors());
@@ -10,6 +10,20 @@ app.get("/api/saludo", (req, res) => {
     res.json({
         mensaje: "API Turismo System funcionando correctamente"
     });
+});
+app.post("/api/actividades", async (req, res) => {
+    const { nombre, descripcion, precio } = req.body;
+    try {
+        const query = "INSERT INTO actividades (nombre, descripcion, precio) VALUES ($1, $2, $3) RETURNING *";
+        const values = [nombre, descripcion, precio];
+        const result = await pool.query(query, values);
+        const nuevaActividad = result.rows[0];
+        res.status(201).json(nuevaActividad);
+
+    } catch (error) {
+        console.error("Error al crear la actividad:", error);
+        res.status(500).json({ mensaje: "Error interno del servidor" });
+    }
 });
 
 module.exports = app;
